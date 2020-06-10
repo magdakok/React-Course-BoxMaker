e740d1de738dfdb49a4aa53d4cda1d42187222d3
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-10 17:24:54 +0100

    create NewBoxForm

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,10 +1,53 @@
 import React, {Component} from 'react';
 
 class NewBoxForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {color:"", width:"", height:""};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+    }
+
+    handleChange(e){
+        this.setState(
+            {
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+
     render(){
       return (
           <div className="NewBoxForm">
-            <h2>NewBoxForm</h2>
+            <h1>Create a new box</h1>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='color'>Color:</label>
+                <input
+                    id='color'
+                    name='color'
+                    value={this.state.color}
+                    onChange={this.handleChange}
+                    />
+                <label htmlFor='width'>width:</label>
+                <input
+                    id='width'
+                    name='width'
+                    value={this.state.width}
+                    onChange={this.handleChange}
+                    />
+                <label htmlFor='height'>Color:</label>
+                <input
+                    id='height'
+                    name='height'
+                    value={this.state.height}
+                    onChange={this.handleChange}
+                    />
+                <button>CREATE</button>
+            </form>
           </div>
         );
     } 
