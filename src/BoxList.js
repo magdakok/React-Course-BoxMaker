f6515398d33749b4f373a0863d954eaa5dd69db4
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-10 19:28:49 +0100

    fix uuid issue

--- src/BoxList.js
+++ src/BoxList.js
@@ -2,30 +2,35 @@ import React, {Component} from 'react';
 import Box from "./Box";
 import NewBoxForm from './NewBoxForm';
 import "./BoxList.css";
-import uuid from "uuid/v4";
+import { v4 as uuidv4 } from 'uuid';
 
 class BoxList extends Component {
     constructor(props){
         super(props);
         this.state = {
             boxes: [
-                {color:"yellow", width: "200", height: "150", id:uuid()},
+                {color:"yellow", width: "200", height: "150", id: uuidv4() },
             ]
         }
         this.addBox = this.addBox.bind(this);
+        this.removeBox = this.removeBox.bind(this);
     }
 
     addBox(box){
-        let newBox = {...box, id: uuid()}
+        let newBox = {...box, id: uuidv4()}
         this.setState(st =>({
             boxes: [...st.boxes, newBox]
         }))
     }
 
+    removeBox(box){
+
+    }
+
     renderBoxes(){
         return (
             this.state.boxes.map(box =>(
-                <Box color={box.color} width={box.width} height={box.height}/>
+                <Box color={box.color} width={box.width} height={box.height} key={box.id} boxId ={box.id} removeBox={this.removeBox}/>
             ))
         )
     }
@@ -33,7 +38,7 @@ class BoxList extends Component {
     render(){
         return (
             <div className="BoxList">
-                <NewBoxForm addBox={this.addBox}/>
+                <NewBoxForm addBox={this.addBox} />
                 <div className="BoxList-container">
                 {this.renderBoxes()}
                 </div>
