ea2577f84933b3d5fd71b5334801e6df33f18a40
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-10 17:06:28 +0100

    add state and renderBox function

--- src/BoxList.js
+++ src/BoxList.js
@@ -3,15 +3,31 @@ import Box from "./Box";
 import NewBoxForm from './NewBoxForm';
 
 class BoxList extends Component {
-  render(){
-    return (
-        <div className="BoxList">
-            <Box />
-            <NewBoxForm />
-          
-        </div>
-      );
-  } 
+    constructor(props){
+        super(props);
+        this.state = {
+            boxes: [
+                {color:"yellow", width: "200", height: "150"}
+            ]
+        }
+    }
+
+    renderBoxes(){
+        return (
+            this.state.boxes.map(box =>(
+                <Box color={box.color} width={box.width} height={box.height}/>
+            ))
+        )
+    }
+
+    render(){
+        return (
+            <div className="BoxList">
+                {this.renderBoxes()}
+                <NewBoxForm />
+            </div>
+        );
+    } 
 }
 
 export default BoxList;
